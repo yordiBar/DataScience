3dee5af3a4c32e4c786bc28015700a629567826d
Bart <bart.rybak@hotmail.com>
2021-03-16 23:24:07 +0000

    replace missing values

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print(x)
 print(y)
+
+# replace missing values with an average of column values
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+print(x)
